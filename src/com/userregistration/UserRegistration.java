5849638e578eb24286a6a348194e19c39809aa2d
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-30 15:41:34 +0530

    ADDED: Valid user name

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,7 +1,23 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your first name");
+		String firstName = sc.next();
+		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}");
+		Matcher matcher = pattern.matcher(firstName);
+		boolean matches = matcher.find();
+		if (matches) {
+			System.out.println("your name is valid");
+		} else {
+			System.out.println("please enter a valid name");
+		}
 	}
 }
