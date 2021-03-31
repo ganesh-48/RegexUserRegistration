cf16e96d450825629d3cd58bf1870ef8b1c073b6
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-31 07:32:46 +0530

    ADDED: User give pre-defined password

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -47,9 +47,21 @@ public class UserRegistration {
 		pattern = Pattern.compile("^((\\\\+)?(\\\\d{2}[-]))?(\\\\d{10}){1}?$",Pattern.CASE_INSENSITIVE);
 		matcher = pattern.matcher(number);
 		matches = matcher.find();
-		if (matches)
+		if (matches) {
 			System.out.println("your mobile number is valid");
-		else
+		}else {
 			System.out.println("please enter a valid mobile number");
+		}
+	
+		System.out.println("enter your password");
+		String password = sc.next();
+		pattern = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(password);
+		matches = matcher.find();
+		if(matches) {
+			System.out.println("password is valid");
+		}else {
+			System.out.println("enter your valid password");
+		}
 	}
 }
